373041ac11712e1501f51e82479322356dc2ebf7
Yan Kalinin <toxword.yk@gmail.com>
2021-04-02 14:58:24 +0300

    Added checkbox reading from form

--- src/main/java/by/poynac/shop/controller/CatalogController.java
+++ src/main/java/by/poynac/shop/controller/CatalogController.java
@@ -1,10 +1,10 @@
 package by.poynac.shop.controller;
 
+import by.poynac.shop.model.AttributeFilterWrapper;
 import by.poynac.shop.model.Product;
 import by.poynac.shop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
@@ -23,24 +24,26 @@ public class CatalogController {
     private ProductRepository productRepository;
 
     @GetMapping
-    public String catalog(@RequestParam(defaultValue = "0") String page,
+    public String catalog(HttpServletRequest request,
+                          @RequestParam(defaultValue = "0") String page,
                           @CookieValue(value = "size", defaultValue = "6") String pageSize,
-                          @CookieValue(value = "filter", defaultValue = "def") String filter,
+                          @CookieValue(value = "sortItem", defaultValue = "def") String sortItem,
                           @CookieValue(value = "sortOption", defaultValue = "asc") String sortOption,
                           Model model) {
         Page<Product> products = productRepository.findAll(PageRequest.of(Integer.parseInt(page),
-                Integer.parseInt(pageSize), filterSort(filter, sortOption)));
+                Integer.parseInt(pageSize), filterSort(sortItem, sortOption)));
         model.addAttribute("curPage", Integer.parseInt(page));
         model.addAttribute("products", products);
         model.addAttribute("size", Integer.parseInt(pageSize));
-        model.addAttribute("filter", filter);
+        model.addAttribute("sortItem", sortItem);
         model.addAttribute("sort", sortOption);
+        model.addAttribute("wrapper", new AttributeFilterWrapper());
         return "catalog/catalog";
     }
 
-    public Sort filterSort(String filter, String sortOption) {
+    public Sort filterSort(String sortItem, String sortOption) {
         String sortingType;
-        switch (filter) {
+        switch (sortItem) {
             case "alphabet":
                 sortingType = "name";
                 break;
@@ -51,27 +54,29 @@ public class CatalogController {
                 sortingType = "id";
                 break;
         }
-        if (sortOption.equals("asc")) {
-            return Sort.by(Sort.Order.asc(sortingType));
-        } else {
-            return Sort.by(Sort.Order.desc(sortingType));
-        }
+        return sortOption.equals("asc")
+                ? Sort.by(Sort.Order.asc(sortingType))
+                : Sort.by(Sort.Order.desc(sortingType));
+
     }
 
     @PostMapping
-    public String setCatalogSize(HttpServletResponse response,
+    public String setCatalogSize(HttpServletRequest request,
+                                 HttpServletResponse response,
                                  @RequestParam(required = false) String size,
-                                 @RequestParam(required = false) String filter,
-                                 @RequestParam(required = false) String sortOption) {
+                                 @RequestParam(required = false) String sortItem,
+                                 @RequestParam(required = false) String sortOption,
+                                 @ModelAttribute(value = "wrapper") AttributeFilterWrapper wrapper) {
         if (size != null) {
             response.addCookie(new Cookie("size", size));
         }
-        if (filter != null) {
-            response.addCookie(new Cookie("filter", filter));
+        if (sortItem != null) {
+            response.addCookie(new Cookie("sortItem", sortItem));
         }
         if (sortOption != null) {
             response.addCookie(new Cookie("sortOption", sortOption));
         }
+
         return "redirect:/catalog";
     }
 
--- src/main/java/by/poynac/shop/repository/ProductRepository.java
+++ src/main/java/by/poynac/shop/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package by.poynac.shop.repository;
 
 import by.poynac.shop.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findProductsByAttributes_Value(String value, Pageable pageable);
 
 }
