be58105b147e51cdc2840ffefde0d23efd39e837
Yan Kalinin <toxword.yk@gmail.com>
2021-04-19 13:33:07 +0300

    Added basic security

--- src/main/java/by/poynac/shop/PoynacShopApplication.java
+++ src/main/java/by/poynac/shop/PoynacShopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -36,5 +37,11 @@ public class PoynacShopApplication implements WebMvcConfigurer {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         registry.addInterceptor(localeChangeInterceptor);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("home/login");
+        registry.addViewController("/profile").setViewName("home/profile");
     }
 }
